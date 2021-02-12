750a1c69db62c39c82b7652955d601d707a7f597
Diogo Fragoso <diogofragoso@yahoo.com.br>
2021-02-12 12:30:28 -0300

    Exemplo caso ok

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       string dia = " ";
+            Console.WriteLine("Dias da semana");
+
+
+            int x = int.Parse(Console.ReadLine());
+            switch (x) {
+
+                case 1:
+                    dia = "Domingo";
+                    break;       
+                case 2:
+                dia = "Segunda";
+                break;
+
+                case 3:
+                dia = "Terça"    ;
+                break;
+                
+                 default:
+                dia = " Valor inválido";
+                break;
+            }
+            System.Console.WriteLine("Dia da semana " + dia);
+
+            
+
+        }
+    }
+}
